e777e1c2166137438274618ed126545f3e302f06
EsheBT <eshetu9244@yahoo.com>
2023-03-15 11:14:42 -0700

    best

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,9 +1,11 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
-    if a_dictionary is None or a_dictionary == {}:
+    if not a_dictionary:
         return None
-    m = sorted(a_dictionary.values())
-    value_max = m[-1]
-    for i in a_dictionary:
-        if value_max == a_dictionary[i]:
-            return i 
+    biggest = 0
+    for key, value in a_dictionary.items():
+        if value > biggest:
+            biggest = value
+    for key, value in a_dictionary.items():
+        if value == biggest:
+            return key 
