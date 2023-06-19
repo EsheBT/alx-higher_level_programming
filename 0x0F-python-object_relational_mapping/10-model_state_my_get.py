c5ae75b65e757d3bb74b023bd2a53411d0129be3
EsheBT <eshetu9244@yahoo.com>
2023-06-19 03:46:13 -0700

    t10

--- 0x0F-python-object_relational_mapping/10-model_state_my_get.py
+++ 0x0F-python-object_relational_mapping/10-model_state_my_get.py
@@ -1,20 +1,26 @@
 #!/usr/bin/python3
-""" prints the State object with the name passed as argument from the database
+"""Script prints State object with the name passed as argument
+Takes three arguments
+    mysql username
+    mysql password
+    database name
+Connects to host localhost and default port (3306)
 """
-import sys
-from model_state import Base, State
-from sqlalchemy import (create_engine)
-from sqlalchemy.orm import sessionmaker
-
-
 if __name__ == "__main__":
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'
-                           .format(sys.argv[1], sys.argv[2], sys.argv[3]))
+    from sqlalchemy import (create_engine)
+    from sqlalchemy.orm import sessionmaker
+    from model_state import Base, State
+    from sys import argv
+    Session = sessionmaker()
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
+        argv[1], argv[2], argv[3]), pool_pre_ping=True)
+    session = Session(bind=engine)
     Base.metadata.create_all(engine)
-    Session = sessionmaker(bind=engine)
-    session = Session()
-    instance = session.query(State).filter(State.name == (sys.argv[4],))
-    try:
-        print(instance[0].id)
-    except IndexError:
+    instances = session.query(State).filter(State.name == argv[4]).all()
+    if instances:
+        for instance in instances:
+            if instance.name == argv[4]:
+                print("{}".format(instance.id))
+    else:
         print("Not found")
+    session.close()
