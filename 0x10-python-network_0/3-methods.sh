afe6b402d2cd77ccb118f56cf35cbab857b1f062
EsheBT <eshetu9244@yahoo.com>
2023-06-30 12:39:48 -0700

    T3

--- 0x10-python-network_0/3-methods.sh
+++ 0x10-python-network_0/3-methods.sh
@@ -1,4 +1,3 @@
-
 #!/bin/bash
-# takes in a URL and displays all HTTP methods the server will accept.
-curl -Is "$1" | grep "Allow:" | cut -d ":" -f 2 | cut -c 2- | rev | cut -c 2- | rev
+# script that takes in a URL and displays all HTTP methods the server will accept
+curl -s -I -X OPTIONS "$1" | grep "Allow:" | cut -f2- -d" ""
