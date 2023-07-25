b49255d3890df233ac9251525923a78fc3ee0137
EsheBT <eshetu9244@yahoo.com>
2023-07-25 11:37:25 -0700

    t0

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,11 +1,12 @@
 #!/usr/bin/node
-/*
-    reads and prints the content of a file
-*/
+const process = require('process');
 const fs = require('fs');
-const nameFile = process.argv[2];
 
-fs.readFile(nameFile, 'utf8', function (err, data) {
+// The first argument is the file path
+const file = process.argv[2];
+// The content of the file must be written in utf-8
+fs.readFile(file, 'utf8', function (err, data) {
+  // If an error occurred during the reading, print the error object
   if (err) {
     console.log(err);
   } else {
