bac55ecb88b92909cc5510a1a6d4772c0f348f9f
EsheBT <eshetu9244@yahoo.com>
2023-07-26 05:16:10 -0700

    t6

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -1,16 +1,23 @@
 #!/usr/bin/node
+// script that display the status code of a GET request.
+
+const URL = process.argv[2];
 const request = require('request');
-request(process.argv[2], function (error, response, body) {
-  if (!error) {
-    const todos = JSON.parse(body);
-    let completed = {};
-    todos.forEach((todo) => {
-      if (todo.completed && completed[todo.userId] === undefined) {
-        completed[todo.userId] = 1;
-      } else if (todo.completed) {
-        completed[todo.userId] += 1;
+request(URL, function (err, response, body) {
+  if (!err && response.statusCode === 200) {
+    const json = JSON.parse(body);
+    const task = {};
+    for (const x in json) {
+      const data = json[x];
+      if (data.completed === true) {
+        const id = data.userId;
+        if (id in task) {
+          task[id]++;
+        } else {
+          task[id] = 1;
+        }
       }
-    });
-    console.log(completed);
+    }
+    console.log(task);
   }
 });
